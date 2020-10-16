bf853d07a3489f0c7a03764a7de75e8821ec8f70
Antonio Sein Jagou <antonioseinjagou95@gmail.com>
2020-10-16 11:49:44 -0600

    Skull Crasher Alpha
    - Aim added
    - Button Added
    - Shoot Script
    - Skull Model
    - Material Created

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject arCamera;
+    public GameObject smoke;
+
+    public int Score;
+
+    public Text scoreText;
+
+    public void shoot() {
+        RaycastHit hit;
+
+        if(Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit)) {
+            if (hit.transform.name == "whiteSkull(Clone)") { //Esto es para especificar que va a pasar si el jugador "dispara" contra cierto game object del juego
+                Destroy(hit.transform.gameObject);
+
+                Instantiate(smoke, hit.point, Quaternion.LookRotation(hit.normal));
+                AddScore();
+            }else
+            if (hit.transform.name == "blueSkull(Clone)") { //Esto es para especificar que va a pasar si el jugador "dispara" contra cierto game object del juego
+                Destroy(hit.transform.gameObject);
+
+                Instantiate(smoke, hit.point, Quaternion.LookRotation(hit.normal));
+                AddScoreBlue();
+            }
+
+
+        }
+
+        
+
+        
+    }
+
+    void AddScore()
+    {
+        Score++;
+        scoreText.text = Score.ToString();
+    }
+
+    void AddScoreBlue()
+    {
+        Score+=2;
+        scoreText.text = Score.ToString();
+    }
+    
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public Transform[] spawnPoint; //Puntos donde se hara el spawn
+    public GameObject[] ballons; //Gameobject de globos
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Startspawn());
+    }
+
+  IEnumerator Startspawn() //Numerador que hara spawnear los globos
+  {
+      yield return new WaitForSeconds(4);
+
+      for (int i = 0; i < 3; i++)
+      {
+          Instantiate(ballons[i], spawnPoint[i].position, Quaternion.identity);
+      }
+
+      StartCoroutine(Startspawn());
+  }
+}
